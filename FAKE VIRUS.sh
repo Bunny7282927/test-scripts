8e98059515a1025d9ee26e1f2665b53bd472dd95
Bunny7282927 <107234358+Bunny7282927@users.noreply.github.com>
2022-07-07 20:33:17 +0530

    Create FAKE VIRUS.sh

---
+++ FAKE VIRUS.sh
@@ -0,0 +1,26 @@
+
+echo STARTING......
+sleep 1
+echo ██╗░░░██╗██╗██████╗░██╗░░░██╗░██████╗
+echo ██║░░░██║██║██╔══██╗██║░░░██║██╔════╝
+echo ╚██╗░██╔╝██║██████╔╝██║░░░██║╚█████╗░
+echo ░╚████╔╝░██║██╔══██╗██║░░░██║░╚═══██╗
+echo ░░╚██╔╝░░██║██║░░██║╚██████╔╝██████╔╝
+echo ░░░╚═╝░░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░
+sleep 2
+echo Your Linux/Pc is infected with dangerous virus!
+sleep 0.5
+echo Closing this tab may give your pc an dangerous file.!
+sleep 2
+echo Do you want to enjoy last minute of your pc?
+sleep 1
+echo Type in the nano 
+sleep 1
+nano virus.txt
+sleep 5
+echo Enjoy! The last minute
+sleep 1
+echo NOTHING
+
+
+
